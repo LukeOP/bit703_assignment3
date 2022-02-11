a2971b4f22bd46eca52c429fcb802df917f55640
Luke Noble <luke.noble91@hotmail.com>
2022-02-12 11:35:31 +1300

    Massive info dump

---
+++ plugins/openpolytechnic/philter/updates/builder_table_delete_openpolytechnic_philter_images.php
@@ -0,0 +1,21 @@
+<?php namespace Openpolytechnic\Philter\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableDeleteOpenpolytechnicPhilterImages extends Migration
+{
+    public function up()
+    {
+        Schema::dropIfExists('openpolytechnic_philter_images');
+    }
+    
+    public function down()
+    {
+        Schema::create('openpolytechnic_philter_images', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+        });
+    }
+}
